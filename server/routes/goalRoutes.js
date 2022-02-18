380f4a4a1055e0ed6300ea09b906adf197263064
Mukul Sharma <mymukul112@gmail.com>
2022-02-18 13:23:31 +0530

    basic routes setup

---
+++ server/routes/goalRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.status(200).json({ message: "Get Goals" });
+});
+
+router.post("/", (req, res) => {
+  res.status(200).json({ message: "Set Goals" });
+});
+
+router.put("/:id", (req, res) => {
+  res.status(200).json({ message: `Update Goal ${req.params.id}` });
+});
+
+router.delete("/:id", (req, res) => {
+  res.status(200).json({ message: `Delete Goal ${req.params.id}` });
+});
+
+module.exports = router;
