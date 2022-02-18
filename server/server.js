57a82b16381739124067d4ebd661fc18f08fbb6c
Mukul Sharma <mymukul112@gmail.com>
2022-02-18 13:23:16 +0530

    server setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,11 @@
-console.log("Server");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.listen(port, () => {
+  console.log("Server is started at", port);
+});
