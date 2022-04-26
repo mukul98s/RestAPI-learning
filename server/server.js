074389eb3d46e8ff00726a863f7e4bc9c454bd6f
Mukul Sharma <mymukul112@gmail.com>
2022-04-26 16:29:12 +0530

    server started file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
+const colors = require("colors");
+
+const {connectDB} = require("./config/db");
+
+connectDB();
+const {errorHandler} = require("./middleware/errorMiddleware");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/user", require("./routes/userRoutes"));
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log("Server is started at", port);
